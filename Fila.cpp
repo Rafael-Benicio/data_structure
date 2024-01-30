bf1238ab0f6713fb490a208da35637c0cae4560e
Rafael-Benicio <jerobenicio@gmail.com>
2024-01-30 13:13:54 -0300

    Refact: static_queue

--- Fila.cpp
+++ Fila.cpp
@@ -3,58 +3,77 @@
 
 #define MAX 50
 
-typedef int TIPOCHAVE;
+typedef int KEY_TYPE;
 
-typedef struct{
-	TIPOCHAVE chave;
-}REGISTRO;
+class Register{
+public:
+	KEY_TYPE key;
+};
 
-typedef struct{
-	REGISTRO A[MAX];
-	int inicio;
-	int nroElem;
-}FILA;
+class Queue{
+public:
+	Register arrayQueue[MAX];
+	int start;
+	int size;
 
-void inicializaFila(FILA* f){
-	f->inicio=0;
-	f->nroElem=0;
-}
-
-int tamanhoFila(FILA* f){
-	return f->nroElem;
-}
-
-void exibirFila(FILA* f){
-	printf("Fila: ");
-	int i=f->inicio;
-	int temp;
-	for (temp=0;temp<f->nroElem;temp++){
-		printf("%i ",f->A[i].chave);
-		i=(i+1)%MAX;
+	Queue(){
+		this->start=0;
+		this->size=0;
 	}
-}
+	int lenght(){
+		return this->size;
+	}
 
-bool inserirElemento(FILA*f,REGISTRO*reg){
-	if(f->nroElem>=MAX) return false;
-	int posicao=(f->inicio+f->nroElem)%MAX;
-	f->A[posicao]=reg;
-	f->nroElem++;
-	return true;
-}
+	void show(){
+		printf("Queue:");
+		int i=this->start;
+		int temp;
+		for (temp=0;temp<this->size;temp++){
+			printf(" %i |",this->arrayQueue[i].key);
+			i=(i+1)%MAX;
+		}
+		printf("\n");
+	}
 
-bool excluiElementoFila(FILA*f,REGISTRO* reg){
-	if(f->nroElem==0) return false;
-	*reg=f->A[f->inicio];
-	f->inicio=(f->inicio+1)%MAX;
-	return true;
-}
+	bool insert(KEY_TYPE reg){
+		if(this->size>=MAX) return false;
+		int posicao=(this->start+this->size)%MAX;
+		this->arrayQueue[posicao]=Register{reg};
+		this->size++;
+		return true;
+	}
 
-void reinicializa(FILA*f){
-	inicializaFila(f);
-}
+	bool pop(Register* reg){
+		if(this->size==0) return false;
+		*reg=this->arrayQueue[this->start];
+		this->start=(this->start+1)%MAX;
+		size--;
+		return true;
+	}
+
+	void reinicialize(){
+		this->start=0;
+		this->size=0;
+	}
+};
+
+
 
 int main(){
-
+	Queue my_queue;
+	my_queue.show();
+	my_queue.insert(1);
+	my_queue.insert(3);
+	my_queue.insert(65);
+	my_queue.insert(62);
+	my_queue.show();
+	printf("%i :lenght\n",my_queue.lenght());
+	Register my_resiter;
+	my_queue.pop(&my_resiter);
+	printf("%i\n",my_resiter.key);
+	my_queue.show();
+	my_queue.reinicialize();
+	my_queue.show();
 
 	return 0;
 }
